dab6eb53983d0be5b702362422eed84dd088badd
Marks Polakovs <marks@markspolakovs.me>
2020-10-22 13:45:44 +0100

    Initialize empty LibGDX project

---
+++ desktop/src/marlin/auber/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package marlin.auber.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import marlin.auber.AuberGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AuberGame(), config);
+	}
+}
