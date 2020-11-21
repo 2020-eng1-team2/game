da025b9d2a00d32559fb38f309c6dbb2ff8e5fa3
Marks Polakovs <marks@markspolakovs.me>
2020-11-21 12:37:08 +0000

    Fix main menu stutter

--- core/src/marlin/auber/AuberGame.java
+++ core/src/marlin/auber/AuberGame.java
@@ -128,13 +128,13 @@ public class AuberGame extends ApplicationAdapter {
 			}
 		}
 
+		// Clear the screen
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		switch (game_state) {
 			case RUNNING:
 				// Tick timers
 				Timer.tickAll();
-				// Clear the screen
-				Gdx.gl.glClearColor(0, 0, 0, 1);
-				Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 				// Tick the systems
 				for (System syst : systems) {
 					syst.tick();
