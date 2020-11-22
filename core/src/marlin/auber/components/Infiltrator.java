aca4c9e9bb77903450fd587ed62746879c5dbb6d
JJH <62762383+JJH47E@users.noreply.github.com>
2020-11-22 16:32:37 +0000

    Infiltrator Component
    Attach this component to an infiltrator

---
+++ core/src/marlin/auber/components/Infiltrator.java
@@ -0,0 +1,15 @@
+package marlin.auber.components;
+
+import marlin.auber.common.Component;
+
+/**
+ * A component for an Entity which tracks whether or not they are an infiltrator.
+ *
+ * There should be a max of one of these per entity.
+ */
+public class Infiltrator extends Component {
+
+    public Infiltrator() {
+
+    }
+}
