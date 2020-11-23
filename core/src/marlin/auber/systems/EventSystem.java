779eda069896ec40273318d5f1cb15088d843e4b
JJH <62762383+JJH47E@users.noreply.github.com>
2020-11-23 14:46:53 +0000

    Added event starters

--- core/src/marlin/auber/systems/EventSystem.java
+++ core/src/marlin/auber/systems/EventSystem.java
@@ -1,9 +1,16 @@
 package marlin.auber.systems;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import marlin.auber.common.Component;
 import marlin.auber.common.Entity;
 import marlin.auber.common.System;
-import marlin.auber.components.Infiltrator;
-import marlin.auber.components.KeypadTarget;
+import marlin.auber.components.*;
+import marlin.auber.models.World;
+
+import java.util.List;
+import java.util.Random;
 
 public class EventSystem implements System {
 
@@ -17,13 +24,44 @@ public class EventSystem implements System {
      */
     private boolean keypadFixed = false;
 
+    /**
+     * Runs code to being an event once
+     */
+    private boolean startEvent = false;
+
+    private boolean eventPart1 = false;
+
+    private List<Component> abilities;
+
+    public EventSystem() {
+        abilities.add(new SpeedAbility());
+        abilities.add(new InvisAbility());
+        abilities.add(new StunAbility());
+    }
+
     public void tick() {
+        ActivePlayerCharacter player = Entity.getAllEntitiesWithComponents(ActivePlayerCharacter.class).get(0).getComponent(ActivePlayerCharacter.class);
         // Update event status
         updateArrests();
         updateKeypad();
         // Check is event is happening
         if (noEvent()) {
             // No event is happening, start event
+            this.startEvent = true;
+        }
+        else if (player.meltdownTime.isOver()) {
+            // Player lost, ship destroyed. End game
+            // TODO: Game over
+        }
+        if (this.startEvent) {
+            this.startEvent = false;
+            this.eventPart1 = true;
+            startKeypadEvent();
+            // When keypad event ends, start infiltrator event
+        }
+        if (eventPart1 == true) {
+            this.eventPart1 = false;
+            startInfiltratorEvent();
         }
     }
 
@@ -55,5 +93,39 @@ public class EventSystem implements System {
         }
         // No broken keypads on map
         this.keypadFixed = true;
+    }
+
+    private void startKeypadEvent() {
+        // RNG pick a keypad and break it
+        Random random = new Random();
+        int rng = random.nextInt(World.getWorld().map.keypads.size());
+        Entity.getAllEntitiesWithComponents(KeypadTarget.class).get(rng).getComponent(KeypadTarget.class).breakPad();
+    }
+
+    private void startInfiltratorEvent() {
+        // Spawn in infiltrator away from Auber
+        Vector2 max = Vector2.Zero;
+        Vector2 auber = Entity.getAllEntitiesWithComponents(ActivePlayerCharacter.class).get(0).getComponent(Position.class).position;
+        for (Vector2 pos : World.getWorld().map.keypads) {
+            if (pos.dst2(auber) > max.dst2(auber)) {
+                max = pos;
+            }
+        }
+        // Generate random ability
+        Random random = new Random();
+        int rng = random.nextInt(3);
+        Entity.create(
+                "Infiltrator",
+                new Position(max),
+                new AABB(1.8f, 1.8f, AABB.TAG_RENDER | AABB.TAG_COLLISION_X_ONLY),
+                new Walking(),
+                new NPCAI(3.0f),
+                new Renderer(8),
+                new StaticRenderer(
+                        new Texture(Gdx.files.internal("testChar2.png"))
+                ),
+                new Infiltrator(),
+                abilities.get(rng)
+        );
     }
 }
