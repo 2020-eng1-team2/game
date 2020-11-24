c0a61141123bb86e3b168898ac261f4d8a2ad4c0
JJH <62762383+JJH47E@users.noreply.github.com>
2020-11-24 20:57:11 +0000

    New tutorial

--- core/src/marlin/auber/systems/MainMenuSystem.java
+++ core/src/marlin/auber/systems/MainMenuSystem.java
@@ -12,9 +12,11 @@ import marlin.auber.models.World;
 public class MainMenuSystem implements System {
     private final Texture title = new Texture(Gdx.files.internal("graphics/title.png"));
     private final Texture tutorial = new Texture(Gdx.files.internal("graphics/tut.png"));
+    private final Texture tutorial2 = new Texture(Gdx.files.internal("graphics/tut2.png"));
     private final SpriteBatch guiBatch = new SpriteBatch();
     private boolean startGame = false;
-    private boolean showTut = false;
+    private boolean showTut1 = false;
+    private boolean showTut2 = false;
 
     private BitmapFont font = Assets.fonts.cnr;
 
@@ -24,9 +26,17 @@ public class MainMenuSystem implements System {
             Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
             guiBatch.begin();
         }
-        if (this.showTut) {
+        if (this.showTut1) {
             // show tut
             World.getWorld().map.scaleGui(tutorial, 0.9f, guiBatch);
+            if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
+                this.showTut2 = true;
+                this.showTut1 = false;
+            }
+        }
+        else if (this.showTut2) {
+            // show tut
+            World.getWorld().map.scaleGui(tutorial2, 0.9f, guiBatch);
             if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
                 this.startGame = true;
             }
@@ -35,16 +45,21 @@ public class MainMenuSystem implements System {
             // show title
             World.getWorld().map.scaleGui(title, 1.0f, guiBatch);
             if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
-                this.showTut = true;
+                this.showTut1 = true;
             }
         }
         guiBatch.end();
     }
 
+    /**
+     * Starts game, resets main menu for next use
+     * @return whether or not the game should begin
+     */
     public boolean checkStartGame() {
         if (this.startGame) {
             this.startGame = false;
-            this.showTut = false;
+            this.showTut1 = false;
+            this.showTut2 = false;
             return true;
         }
         else {return false;}
